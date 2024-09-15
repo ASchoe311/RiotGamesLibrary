e9926134fd6f9111671e6cac95e6452306af6bfc
adamr <adamrschoenfeld311@gmail.com>
2024-09-15 04:02:32 -0700

    Improve postbuild script

--- postbuild.bat
+++ postbuild.bat
@@ -1,2 +1,5 @@
-start /d "C:\Users\adamr\Desktop\Playnite Portable" Playnite.DesktopApp.exe
+if "%1"=="Debug" start /d "C:\Users\adamr\Desktop\Playnite Portable" Playnite.DesktopApp.exe
 @REM Powershell.exe -ExecutionPolicy Bypass -File "C:\Users\adamr\Documents\Coding\Playnite\ATLInt\ATLauncherInstanceImporter\taillog.ps1"
+if "%1"=="Release" (
+    start /D "C:\Users\adamr\AppData\Local\Playnite" Toolbox.exe pack %2 %3
+)
