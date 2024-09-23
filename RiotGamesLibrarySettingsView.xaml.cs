24980b727890fcb85abba04a30dd916e61e16d79
Adam Schoenfeld <adamrschoenfeld311@gmail.com>
2024-09-22 21:10:58 -0700

    Added spacing to settings view

--- RiotGamesLibrarySettingsView.xaml.cs
+++ RiotGamesLibrarySettingsView.xaml.cs
@@ -32,17 +32,6 @@ namespace RiotGamesLibrary
         {
             InitializeComponent();
         }
-        //public class RowConverter : IMultiValueConverter
-        //{
-        //    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        //    {
-        //        return Tuple.Create(values[0], values[1]);
-        //    }
-        //    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //}
     }
 
 }
